69eb6a0fa6b29d98589450e9d3715311cd2c0bad
s1140311-cmd <s1140311@dmhs.kh.edu.tw>
2025-10-17 16:22:15 +0800

    hw2

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int age = 99;
+    double height = 2010;
+    std::string name = "April";
+    bool is_pass =true;
+    std::cout<< "姓名: "<< name <<"身高:"<< height <<std::endl;
+    std::cout<< "年齡: "<<"是否及格: "<<is_pass<<std::endl;
+    return 0;
+}
