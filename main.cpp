edcd88598bd6739df8787273063704271c81a360
s1140311-cmd <s1140311@dmhs.kh.edu.tw>
2025-11-28 16:11:02 +0800

    hw8
    hw8

--- main.cpp
+++ main.cpp
@@ -2,28 +2,12 @@
 using namespace std;
 
 int main() {
-    double height, weight, bmi;
+    int sum = 0;
 
-    cout << "請輸入身高(cm): ";
-    cin >> height;
+    for (int i = 1; i <= 5; i++) {
+        sum += i;   // sum = sum + i
+    }
 
-    cout << "請輸入體重(kg): ";
-    cin >> weight;
-
-    // 將身高轉換為公尺
-    height = height / 100.0;
-
-    // 計算 BMI
-    bmi = weight / (height * height);
-
-    cout << "您的 BMI = " << bmi << endl;
-    
-    if (bmi < 18.5) {
-    cout << "你可能過輕" << endl;
-} else if (bmi < 24.9) {
-    cout << "你可能正常" << endl;
-} else {
-     cout << "你可能過重"<< endl;
-}
-   return 0;
+    cout << "1 + 2 + 3 + 4 + 5 = " << sum << endl;
+    return 0;
 }
