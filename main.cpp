0a3f3284faf5789aa66ad9c78fa091692b2daa6e
s1140311-cmd <s1140311@dmhs.kh.edu.tw>
2025-12-04 09:39:34 +0800

    hw9
    hw9

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 int main() {
-    int sum = 0;
-
-    for (int i = 1; i <= 5; i++) {
-        sum += i;   // sum = sum + i
+    // 外層迴圈控制行數，從 1 到 9
+    for (int i = 1; i <= 9; i++) {
+        // 內層迴圈控制列數，從 1 到 9
+        for (int j = 1; j <= 9; j++) {
+            // 打印每個乘積，並排版整齊
+            cout << i << " x " << j << " = " << i * j << "\t";
+        }
+        // 換行
+        cout << endl;
     }
-
-    cout << "1 + 2 + 3 + 4 + 5 = " << sum << endl;
     return 0;
 }
